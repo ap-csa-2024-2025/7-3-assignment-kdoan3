c8ebbddd4de9296a08eebbf07d4b7b5cee10ede1
kdoan3 <kbdoan103@student.hbuhsd.edu>
2025-02-21 18:57:21 +0000

    fdfdsa

--- Main.java
+++ Main.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 
 public class Main
 {
+
   public static void main(String[] args)
   {
-    // Feel free to test code here
+    ArrayList<String> name = new ArrayList<String>();
+  name.add("a");
+  name.add("b");
+  name.add("c");
+  name.add("d");
+  name.add("e");
+  name.add("f");
   }
 
   public static void shiftLeft(ArrayList<String> arr)
   {
-    
+    String front = arr.remove(0);
+    arr.add(front);
   }
 
-  public static printStatistics(final ArrayList<Integer> arr)
+  public static void printStatistics(final ArrayList<Integer> arr)
   {
     
   }
