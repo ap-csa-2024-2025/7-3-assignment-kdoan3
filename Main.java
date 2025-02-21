38943ecceac7c7b9e75ab7ad97a8112d8b6a61a3
kdoan3 <kbdoan103@student.hbuhsd.edu>
2025-02-21 19:21:00 +0000

    fdsfdsa

--- Main.java
+++ Main.java
@@ -25,14 +25,41 @@ public class Main
   {
     double sum = 0;
     int count = 0;
+    int maxcount = 0;
     int mode = arr.get(0);
 
     for (Integer num : arr)
     {
       sum += num;
-    }
-    System.out.println("Sum: " + sum);
-    System.out.println("Average: " + sum/arr.length);
+      count = 0;
 
+      for (Integer temp : arr)
+      {
+        if (temp.equals(num))
+        {
+          count++
+        }
+      }
+
+      if (count > maxcount)
+      {
+        mode = num;
+        maxcount = count;
+      }
+
+    }
+
+    if (there is only one mode)
+    {
+      System.out.println("Sum: " + sum);
+      System.out.println("Average: " + sum/arr.size());
+      System.out.println("Mode: " + mode);
+    }
+    else
+    {
+      System.out.println("Sum: " + sum);
+      System.out.println("Average: " + sum/arr.size());
+      System.out.println("Mode: no single mode");
+    }
   }
 }
